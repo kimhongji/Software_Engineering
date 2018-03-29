061597649923ed17f01b225952951796720dede2
jihyun <wjdwl1747@naver.com>
2018-03-29 22:40:14 +0900

    conflict test1 -jh

--- git_test.py
+++ git_test.py
@@ -6,10 +6,5 @@ for i in range(101) :
     if (i%5) == 0 :
             print(i)
 
-<<<<<<< HEAD
 #git branch test 2- kimhongji
 #git branch test 3- hongji
-=======
-#git branch test 2- jihyun
-#git branch test 3- jihyun
->>>>>>> 730c3407972a3207d0278b9745c222fd49b4968e
