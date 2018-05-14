208c5b1990cb883d56436ad6eeb2693ec3667d22
Kkenr <yooop89@gmail.com>
2018-05-14 16:29:31 +0900

    complete

--- Project2/main.c
+++ Project2/main.c
@@ -7,7 +7,7 @@
 struct stat stat1, stat2;
 struct tm *time1, *time2;
 int hour1, hour2, min1, min2;
-
+int day1,day2, mon1,mon2;
 void filestat1(void);
 void filestat2(void);
 void filetime1(void);
@@ -54,24 +54,51 @@ void filetime2(void){
 	min2 = time2->tm_min;
 }
 void sizecmp(void){
+	int size1 = stat1.st_size;
+	int size2 = stat2.st_size;
+	printf("size compare\n");
+	if(size1 > size2)
+		printf("text1 is bigger\n");
+	else if(size1<size2)
+		printf("text2 is bigger\n");
+	else
+		printf("sizes are equal\n");
+	printf("\n");
 }
 void blockcmp(void){
-   int blk1 = &stat1.st_blocks;
-   int blk2 = &stat2.st_blocks;
+  	int blk1 = (int)stat1.st_blocks;
+   	int blk2 = (int)stat2.st_blocks;
 
-   printf("block compare\n");
+   	printf("block compare\n");
 
-   if(blk1 > blk2)
-      printf("text1 is bigger\n");
-   else if(blk2 > blk1)
-      printf("text2 is bigger\n");
-   else
-      printf("sizes are equal\n");
-
-   printf("\n");
-
+   	if(blk1 > blk2)
+      		printf("text1 is bigger\n");
+   	else if(blk1<blk2)
+      		printf("text2 is bigger\n");
+   	else
+      		printf("sizes are equal\n");
+   	printf("\n");
 }
 void datecmp(void){
+	mon1 = time1->tm_mon+1;
+	day1 = time1->tm_mday;
+	mon2 = time2->tm_mon+1;
+	day2 = time2->tm_mday;
+
+	printf("date compare\n");	
+	if(mon1>mon2)
+		printf("text2 is early\n");
+	else if(mon1<mon2)
+		printf("text1 is early\n");
+	else {
+		if(day1>day2)
+			printf("text2 is early\n");
+		else if(day1<day2)
+			printf("text1 is early\n");
+		else
+			printf("same date\n");
+	}
+	printf("\n");
 }
 void timecmp(void){
 	printf("time compare\n");
