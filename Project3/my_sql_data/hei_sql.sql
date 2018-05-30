71206e0c6b666865f3d32973ec57f79b74abd2f3
dhd96 <dhd96@naver.com>
2018-05-30 15:50:04 +0900

    data add

--- Project3/my_sql_data/hei_sql.sql
+++ Project3/my_sql_data/hei_sql.sql
@@ -1,20 +1,10 @@
-use sbtour;
-
+/*  부산이도시에 해당하는 것만 
 select * from package where tour_id in
 		(select tour_id from tour where city_id in
 			(select city_id from city where city_name = "부산" )); 
-	
+			*/
 
-select * from city;
-
-select * from country;
-
-select * from package;
-
-select * from seller;
-
-select * from customer;
-
+/*국내 인것만 */			
 select * from package where tour_id in
 		(select tour_id from tour where country_id in
-			(select country_id from city where country_category = "대한민국" ));
+			(select country_id from country where country_category = "대한민국" )); 
--- Project3/my_sql_data/sbtour-data.sql
+++ Project3/my_sql_data/sbtour-data.sql
@@ -17,7 +17,19 @@ VALUES
  (008,'제주');
 COMMIT;
 
-
+SET AUTOCOMMIT=0;
+INSERT INTO city (city_id, city_name)
+VALUES 
+ (009,'상하이'),
+ (010,'오사카'),
+ (011,'도쿄'),
+ (012,'벤쿠버'),
+ (013,'다낭'),
+ (014,'모스크바'),
+ (015,'바스코'),
+ (016,'런던');
+COMMIT;
+/*--------------------------------------------------------*/
 SET AUTOCOMMIT=0;
 INSERT INTO  country (country_id, country_category, country_name)
 VALUES 
@@ -36,6 +48,12 @@ VALUES
 COMMIT;
 
 SET AUTOCOMMIT=0;
+INSERT INTO  country (country_id, country_category, country_name)
+VALUES 
+ (13, '일본', '일본');
+COMMIT;
+/*--------------------------------------------------------*/
+SET AUTOCOMMIT=0;
 INSERT INTO tour(tour_id, country_id, city_id)
 VALUES 
  (001, 01, 001),
@@ -48,21 +66,60 @@ VALUES
 COMMIT;
 
 SET AUTOCOMMIT=0;
+INSERT INTO tour(tour_id, country_id, city_id)
+VALUES 
+ (008, 02, 009), /*중국-상하이*/
+ (009, 13, 010), /*일본-오사카*/
+ (010, 13, 011), /*일본-도쿄*/
+ (011, 04, 012), /*캐나다-벤쿠버*/
+ (012, 09, 014), /*러시아-모스크바*/
+ (013, 12, 013); /*베트남-다낭*/
+COMMIT;
+
+/*--------------------------------------------------------------------------------------*/
+
+SET AUTOCOMMIT=0;
 INSERT INTO admin(admin_id, admin_name, admin_passwd)
 VALUES ('admin_1', '난관리자1', '486');
 COMMIT;
-
+/*--------------------------------------------------------------------------------------*/
 SET AUTOCOMMIT=0;
 INSERT INTO customer(customer_id, customer_name, customer_passwd, customer_phone, customer_email, customer_account )
 VALUES ('custromer_1', '난소비자1', '486', '010-1234-1234','sell1@gmmail.com', '국민 110-111-000000');
 COMMIT;
 
 SET AUTOCOMMIT=0;
+INSERT INTO customer(customer_id, customer_name, customer_passwd, customer_phone, customer_email, customer_account )
+VALUES ('custromer_2', '난소비자2', '123', '010-1234-2222','sell2@gmmail.com', '국민 110-222-000000'),
+('custromer_3', '난소비자3', '456', '010-1234-3333','sell3@gmmail.com', '국민 110-333-000000'),
+('custromer_4', '난소비자4', '789', '010-1234-4444','sell4@gmmail.com', '국민 110-444-000000');
+COMMIT;
+/*--------------------------------------------------------------------------------------*/
+SET AUTOCOMMIT=0;
 INSERT INTO seller(seller_id, seller_name, seller_passwd, seller_phone, seller_email, seller_account )
 VALUES ('seller_1', '난판매자1', '486', '010-1234-1234','sell1@gmmail.com', '국민 110-111-000000');
 COMMIT;
 
 SET AUTOCOMMIT=0;
+INSERT INTO seller(seller_id, seller_name, seller_passwd, seller_phone, seller_email, seller_account )
+VALUES ('seller_2', '난판매자2', '123', '010-2222-1234','sell2@gmmail.com', '국민 220-111-000000'),
+('seller_3', '난판매자3', '456', '010-3333-1234','sell3@gmmail.com', '국민 330-111-000000'),
+('seller_4', '난판매자4', '789', '010-4444-1234','sell4@gmmail.com', '국민 440-111-000000');
+COMMIT;
+/*--------------------------------------------------------------------------------------*/
+
+SET AUTOCOMMIT=0;
 INSERT INTO package(package_id, seller_id, tour_id, package_name, package_explanation, package_img, package_start, package_arrive, package_term, package_validity, package_cost, package_closed, package_hit)
 VALUES ('부산여행', '001', 002,'부산놀이','다함께 부산을','busan.jpg','2018/05/17', '2018/05/23', '2박3일', 3, 30, '2018/05/17', 0);
+COMMIT;/*날짜, 기간 선택*/
+
+
+SET AUTOCOMMIT=0;
+INSERT INTO package(package_id, seller_id, tour_id, package_name, package_explanation, package_img, package_start, package_arrive, package_term, package_validity, package_cost, package_closed, package_hit)
+VALUES 
+('상하이여행', 'seller_1', 008,'상하이놀이','다함께 상하이을','sanghae.jpg','2018/05/17', '2018/05/23', '2박3일', 3, 30, '2018/05/17', 0),
+('오사카여행', 'seller_2', 009,'오사카놀이','다함께 오사카을','osaka.jpg','2018/06/04', '2018/06/07', '4박3일', 8, 30, '2018/06/01', 0),
+('도쿄여행', 'seller_2', 010,'도쿄놀이','다함께 도쿄을','tokyo.jpg','2018/06/12', '2018/06/20', '9박8일', 8, 30, '2018/06/05', 0),
+('벤쿠버여행', 'seller_3', 011,'벤쿠버놀이','다함께 벤쿠버을','Vancouver.jpg','2018/07/02', '2018/07/10', '9박8일', 15, 30, '2018/06/30', 0),
+('다낭여행', 'seller_4', 013,'다낭놀이','다함께 다낭을','danang.jpg','2018/07/20', '2018/07/25', '6박5일', 12, 30, '2018/07/15', 0);
 COMMIT;
